504ba10a3212156f205b057d6d79cd074ca6f406
Yasmin Jandriele <97072307+yasjcarvalho@users.noreply.github.com>
2023-08-09 11:55:17 -0300

    reestudando

--- calculadora/cypress.config.js
+++ calculadora/cypress.config.js
@@ -1,14 +1,9 @@
-module.exports = {
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "vite",
-    },
-  },
+const { defineConfig } = require("cypress");
 
+module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-};
+});
